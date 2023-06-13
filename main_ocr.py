ceced031b9a616f981202cabc938d1b5ed6a79d0
Zarivate <Zarivate1@gmail.com>
2023-06-13 17:08:52 -0500

    Got project working on Desktop

--- main_ocr.py
+++ main_ocr.py
@@ -6,7 +6,7 @@ import numpy as np
 
 
 # Load model for English and Japanese
-reader_en_ja = Reader(["en","ja"])
+reader_en_ja = Reader(["en", "ja"])
 
 # Start the counters
 t1_start = perf_counter()
@@ -24,19 +24,17 @@ t1_stop = perf_counter()
 
 
 def read_text(image_name, model_name, in_line=True):
-    #Read the data
-   text = model_name.readtext(image_name, detail=0, paragraph=in_line)
+    # Read the data
+    text = model_name.readtext(image_name, detail=0, paragraph=in_line)
 
-    #Join texts writing each text in new line
-   return "\n".join(text)
+    # Join texts writing each text in new line
+    return "\n".join(text)
 
 
 jp_example_1 = read_text("examples/example_8.jpg", reader_en_ja)
 print("\n" + jp_example_1)
 
 print("Elapsed time:", t1_stop, t1_start)
- 
- 
-print("Elapsed time during the whole program in seconds:",
-                                        t1_stop-t1_start)
 
+
+print("Elapsed time during the whole program in seconds:", t1_stop - t1_start)
